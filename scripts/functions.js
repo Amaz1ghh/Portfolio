eab31aa0ff54baf4eb066df2e87766e731d88117
Amaz1ghh <amazigh35y@gmail.com>
2024-05-23 11:47:46 +0200

    ajout du projets.js

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,3 +1,10 @@
 function openInNewTab(url) {
     window.open(url, '_blank', 'noopener,noreferrer');
 }
+
+function affichePage(event) {
+    // Supprimer la classe 'estSelect' de tous les boutons
+    buttons.forEach(btn => btn.classList.remove('estSelect'));
+    // Ajouter la classe 'estSelect' au bouton cliqué
+    event.target.classList.add('estSelect');
+}
